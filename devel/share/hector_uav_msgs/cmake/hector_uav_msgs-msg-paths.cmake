aa76d7db90216114e697dd0fd62f90fdaecf91ff
John <jkaczma@ncsu.edu>
2024-09-14 19:24:22 -0400

    Issue #1: Initial investigation of attempting to arbitrarily move to a singular location (X,Y,Z).

--- devel/share/hector_uav_msgs/cmake/hector_uav_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(hector_uav_msgs_MSG_INCLUDE_DIRS "/home/john/Desktop/project/hector-moveit/src/hector_quadrotor/hector_uav_msgs/msg;/home/john/Desktop/project/hector-moveit/devel/share/hector_uav_msgs/msg")
-set(hector_uav_msgs_MSG_DEPENDENCIES actionlib_msgs;std_msgs;geometry_msgs)
