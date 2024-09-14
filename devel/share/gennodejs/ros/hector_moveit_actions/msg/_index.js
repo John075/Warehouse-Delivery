ffab9091b9ad4d8ef566305a9a9957d2f9ab3c7e
John <jkaczma@ncsu.edu>
2024-09-14 12:11:10 -0400

    Push of all files I've worked on thus far. Performed a few modifications from hector-moveit, and added a custom world from a prior competition.

---
+++ devel/share/gennodejs/ros/hector_moveit_actions/msg/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let ExecuteDroneTrajectoryAction = require('./ExecuteDroneTrajectoryAction.js');
+let ExecuteDroneTrajectoryGoal = require('./ExecuteDroneTrajectoryGoal.js');
+let ExecuteDroneTrajectoryActionGoal = require('./ExecuteDroneTrajectoryActionGoal.js');
+let ExecuteDroneTrajectoryFeedback = require('./ExecuteDroneTrajectoryFeedback.js');
+let ExecuteDroneTrajectoryActionFeedback = require('./ExecuteDroneTrajectoryActionFeedback.js');
+let ExecuteDroneTrajectoryResult = require('./ExecuteDroneTrajectoryResult.js');
+let ExecuteDroneTrajectoryActionResult = require('./ExecuteDroneTrajectoryActionResult.js');
+
+module.exports = {
+  ExecuteDroneTrajectoryAction: ExecuteDroneTrajectoryAction,
+  ExecuteDroneTrajectoryGoal: ExecuteDroneTrajectoryGoal,
+  ExecuteDroneTrajectoryActionGoal: ExecuteDroneTrajectoryActionGoal,
+  ExecuteDroneTrajectoryFeedback: ExecuteDroneTrajectoryFeedback,
+  ExecuteDroneTrajectoryActionFeedback: ExecuteDroneTrajectoryActionFeedback,
+  ExecuteDroneTrajectoryResult: ExecuteDroneTrajectoryResult,
+  ExecuteDroneTrajectoryActionResult: ExecuteDroneTrajectoryActionResult,
+};
