fdf9a502c2320c4549f4a8698588d6a754f35c91
jkaczma <jkaczma@ncsu.edu>
2024-10-06 20:50:11 -0400

    Fixed node & package name in headless docker ROS launch file

--- drone-software/src/warehouse_delivery/src/explorer_node.cpp
+++ drone-software/src/warehouse_delivery/src/explorer_node.cpp
@@ -4,12 +4,12 @@
 int main(int argc, char** argv)
 {
 
-    ros::init(argc, argv, "hector_explorer");
+    ros::init(argc, argv, "warehouse_delivery");
     ros::AsyncSpinner spinner(1);
     spinner.start();
     ros::NodeHandle node_handle("~");
 
-    ROS_INFO("Explorer Node entered!");
+    ROS_INFO("Warehouse Delivery Node entered!");
     
     Quadrotor quad(std::ref(node_handle));
     quad.takeoff();
